c048a1b8101676603ffbc63d82df966872266c3a
Robert Dickenson <robert.dickenson@gmail.com>
2016-05-19 12:31:04 +1000

    Run astyle to fix travis build fail

--- src/systemcmds/esc/esc.c
+++ src/systemcmds/esc/esc.c
@@ -130,42 +130,49 @@ esc1(void)
 
 		if (updated) {
 			orb_copy(ORB_ID(esc_status), esc_fd, &erb);
-/*
-	uint16_t counter;
-	uint64_t timestamp;
-	uint8_t esc_count;
-	uint8_t esc_connectiontype;
---	struct esc_report_s esc[8];
-		uint8_t esc_vendor;
-		uint32_t esc_errorcount;
-		int32_t esc_rpm;
-		float esc_voltage;
-		float esc_current;
-		float esc_temperature;
-		float esc_setpoint;
-		uint16_t esc_setpoint_raw;
-		uint16_t esc_address;
-		uint16_t esc_version;
-		uint16_t esc_state;
- */
-            printf("esc %u %u %u ", erb.counter, erb.esc_count, erb.esc_connectiontype);
-            printf(": %u %u %u ", erb.esc[0].esc_vendor, erb.esc[0].esc_errorcount, erb.esc[0].esc_rpm);
-            printf(": %u %u %u %u", erb.esc[0].esc_address, erb.esc[0].esc_setpoint_raw, erb.esc[0].esc_version, erb.esc[0].esc_state);
-            printf("\n");
-            printf("esc %8.4f %8.4f", (double)erb.esc[0].esc_voltage, (double)erb.esc[0].esc_current);
-            printf(": %8.4f %8.4f",   (double)erb.esc[0].esc_temperature, (double)erb.esc[0].esc_setpoint);
-            printf("\n");
-        }
+			/*
+				uint16_t counter;
+				uint64_t timestamp;
+				uint8_t esc_count;
+				uint8_t esc_connectiontype;
+			--	struct esc_report_s esc[8];
+					uint8_t esc_vendor;
+					uint32_t esc_errorcount;
+					int32_t esc_rpm;
+					float esc_voltage;
+					float esc_current;
+					float esc_temperature;
+					float esc_setpoint;
+					uint16_t esc_setpoint_raw;
+					uint16_t esc_address;
+					uint16_t esc_version;
+					uint16_t esc_state;
+			 */
+			printf("esc %u %u %u ", erb.counter, erb.esc_count, erb.esc_connectiontype);
+			printf(": %u %u %u ", erb.esc[0].esc_vendor, erb.esc[0].esc_errorcount, erb.esc[0].esc_rpm);
+			printf(": %u %u %u %u", erb.esc[0].esc_address, erb.esc[0].esc_setpoint_raw, erb.esc[0].esc_version,
+			       erb.esc[0].esc_state);
+			printf("\n");
+			printf("esc %8.4f %8.4f", (double)erb.esc[0].esc_voltage, (double)erb.esc[0].esc_current);
+			printf(": %8.4f %8.4f", (double)erb.esc[0].esc_temperature, (double)erb.esc[0].esc_setpoint);
+			printf("\n");
+		}
 
 		struct pollfd fds;
+
 		int ret;
+
 		fds.fd = 0; /* stdin */
+
 		fds.events = POLLIN;
+
 		ret = poll(&fds, 1, 0);
+
 		if (ret > 0) {
 			char c;
 
 			read(0, &c, 1);
+
 			switch (c) {
 			case 0x03: // ctrl-c
 			case 0x1b: // esc
@@ -175,6 +182,7 @@ esc1(void)
 			}
 		}
 	}
+
 	return OK;
 }
 
@@ -191,34 +199,42 @@ esc2(void)
 
 		if (updated) {
 			orb_copy(ORB_ID(esc_report), esc_fd, &erb);
-/*
-	uint8_t esc_vendor;
-	uint32_t esc_errorcount;
-	int32_t esc_rpm;
-	float esc_voltage;
-	float esc_current;
-	float esc_temperature;
-	float esc_setpoint;
-	uint16_t esc_setpoint_raw;
-	uint16_t esc_address;
-	uint16_t esc_version;
-	uint16_t esc_state;
- */
-            printf("esc %u %u %u %u %u %u %u", erb.esc_vendor, erb.esc_errorcount, erb.esc_rpm, erb.esc_address, erb.esc_setpoint_raw, erb.esc_version, erb.esc_state);
-            printf("\n");
-            printf("esc %8.4f %8.4f %8.4f %8.4f", (double)erb.esc_voltage, (double)erb.esc_current, (double)erb.esc_temperature, (double)erb.esc_setpoint);
-            printf("\n");
-        }
+			/*
+				uint8_t esc_vendor;
+				uint32_t esc_errorcount;
+				int32_t esc_rpm;
+				float esc_voltage;
+				float esc_current;
+				float esc_temperature;
+				float esc_setpoint;
+				uint16_t esc_setpoint_raw;
+				uint16_t esc_address;
+				uint16_t esc_version;
+				uint16_t esc_state;
+			 */
+			printf("esc %u %u %u %u %u %u %u", erb.esc_vendor, erb.esc_errorcount, erb.esc_rpm, erb.esc_address,
+			       erb.esc_setpoint_raw, erb.esc_version, erb.esc_state);
+			printf("\n");
+			printf("esc %8.4f %8.4f %8.4f %8.4f", (double)erb.esc_voltage, (double)erb.esc_current, (double)erb.esc_temperature,
+			       (double)erb.esc_setpoint);
+			printf("\n");
+		}
 
 		struct pollfd fds;
+
 		int ret;
+
 		fds.fd = 0; /* stdin */
+
 		fds.events = POLLIN;
+
 		ret = poll(&fds, 1, 0);
+
 		if (ret > 0) {
 			char c;
 
 			read(0, &c, 1);
+
 			switch (c) {
 			case 0x03: // ctrl-c
 			case 0x1b: // esc
@@ -228,6 +244,7 @@ esc2(void)
 			}
 		}
 	}
+
 	return OK;
 }
 
@@ -251,15 +268,18 @@ esc2(void)
 		if (updated) {
 			orb_copy(ORB_ID(esc_report), esc_fd, &erb[i++]);
 		}
+
 #if 0
 		struct pollfd fds;
 		int ret;
 		fds.fd = 0; // stdin
 		fds.events = POLLIN;
 		ret = poll(&fds, 1, 0);
+
 		if (ret > 0) {
 			char c;
 			read(0, &c, 1);
+
 			switch (c) {
 			case 0x03: // ctrl-c
 			case 0x1b: // esc
@@ -268,51 +288,52 @@ esc2(void)
 				return OK;
 			}
 		}
+
 #endif
 	}
 
-/*
-	uint32_t total_time = 0;
-	last_timestamp = erb[0].timestamp;
+	/*
+		uint32_t total_time = 0;
+		last_timestamp = erb[0].timestamp;
 
-	for (i = 0; i < MAX_SAMPLES; i++) {
+		for (i = 0; i < MAX_SAMPLES; i++) {
 
-//			printf("magxyz %d %d %d ", erb[i].x_raw, erb[i].y_raw, erb[i].z_raw);
-			printf("magxyz %8.4f %8.4f %8.4f ", (double)erb[i].x, (double)erb[i].y, (double)erb[i].z);
+	//			printf("magxyz %d %d %d ", erb[i].x_raw, erb[i].y_raw, erb[i].z_raw);
+				printf("magxyz %8.4f %8.4f %8.4f ", (double)erb[i].x, (double)erb[i].y, (double)erb[i].z);
 
-			uint8_t cnt0 = (erb[i].error_count & 0x000000FF00000000) >> 32;
-			uint8_t cnt1 = (erb[i].error_count & 0x00000000FF000000) >> 24;
-			uint8_t cnt2 = (erb[i].error_count & 0x0000000000FF0000) >> 16;
-			uint8_t cnt3 = (erb[i].error_count & 0x000000000000FF00) >> 8;
-			uint8_t cnt4 = (erb[i].error_count & 0x00000000000000FF);
-			printf("[%x %x %x %x %x] ", cnt0, cnt1, cnt2, cnt3, cnt4);
+				uint8_t cnt0 = (erb[i].error_count & 0x000000FF00000000) >> 32;
+				uint8_t cnt1 = (erb[i].error_count & 0x00000000FF000000) >> 24;
+				uint8_t cnt2 = (erb[i].error_count & 0x0000000000FF0000) >> 16;
+				uint8_t cnt3 = (erb[i].error_count & 0x000000000000FF00) >> 8;
+				uint8_t cnt4 = (erb[i].error_count & 0x00000000000000FF);
+				printf("[%x %x %x %x %x] ", cnt0, cnt1, cnt2, cnt3, cnt4);
 
-			uint32_t mag_interval = (erb[i].error_count & 0xFFFFFF0000000000) >> 40;
-			printf("%u  ", mag_interval);
-			if ((mag_interval) < 5000) {
-				printf("* ");
-			}
-			if ((mag_interval) > 15000) {
-				printf("! ");
-			}
+				uint32_t mag_interval = (erb[i].error_count & 0xFFFFFF0000000000) >> 40;
+				printf("%u  ", mag_interval);
+				if ((mag_interval) < 5000) {
+					printf("* ");
+				}
+				if ((mag_interval) > 15000) {
+					printf("! ");
+				}
 
-			uint32_t orb_interval = erb[i].timestamp - last_timestamp;
-			if (orb_interval != mag_interval) {
-				printf("orb ", orb_interval); // missed some orb publications
-			}
-			last_timestamp = erb[i].timestamp;
-			total_time += orb_interval;
+				uint32_t orb_interval = erb[i].timestamp - last_timestamp;
+				if (orb_interval != mag_interval) {
+					printf("orb ", orb_interval); // missed some orb publications
+				}
+				last_timestamp = erb[i].timestamp;
+				total_time += orb_interval;
 
-			if (erb[i].x_raw == erb[i-1].x_raw && erb[i].y_raw == erb[i-1].y_raw && erb[i].z_raw == erb[i-1].z_raw ) {
-				printf("dup "); // duplicate magnetometer xyz data
-			}
+				if (erb[i].x_raw == erb[i-1].x_raw && erb[i].y_raw == erb[i-1].y_raw && erb[i].z_raw == erb[i-1].z_raw ) {
+					printf("dup "); // duplicate magnetometer xyz data
+				}
 
-			printf("\n");
-	}
-	double t = (double)total_time / 1000000.0;
-	printf("total_time: %.3f s\n", t);
- */
-    return OK;
+				printf("\n");
+		}
+		double t = (double)total_time / 1000000.0;
+		printf("total_time: %.3f s\n", t);
+	 */
+	return OK;
 }
 #endif
 
@@ -323,14 +344,18 @@ esc_main(int argc, char *argv[])
 {
 	if (argc < 2) {
 		esc1();
+
 	} else {
 		int reps = atoi(argv[1]);
+
 		if (reps > 20 || reps < 1) {
 			reps = 1;
 		}
+
 		while (reps--) {
 			esc2();
 		}
 	}
+
 	return OK;
 }
